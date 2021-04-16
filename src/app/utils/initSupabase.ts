b988a2b6cfee4867092dda3fe4fadc55a3b67935
Daniel Laera <laera.daniel@hotmail.com>
2021-04-16 11:13:32 +0200

    removed vars

--- src/app/utils/initSupabase.ts
+++ src/app/utils/initSupabase.ts
@@ -1,4 +1,4 @@
 export const initSupabase = {
-    supabaseUrl: 'https://wtldzejlgdlxpvbizgco.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxODA1NzMzNSwiZXhwIjoxOTMzNjMzMzM1fQ.W28mKdWBTfIxXLWlsmn8VJ1cXh8HiEZ7noZ9D1yb0Hk'
+    supabaseUrl: 'your_url',
+    supabaseKey: 'your_key'
 };
